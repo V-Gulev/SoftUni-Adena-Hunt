e40b605d9fa304ff31419f9fadf0c5814d6a3395
V-Gulev <velizargulev@abv.bg>
2025-12-23 17:23:58 +0200

    feat: Add Mob Properties

---
+++ src/main/java/main/property/MobProperties.java
@@ -0,0 +1,35 @@
+package main.property;
+
+import lombok.Data;
+import main.config.YamlPropertySourceFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties
+@PropertySource(value = "mob-details.yaml", factory = YamlPropertySourceFactory.class)
+public class MobProperties {
+
+    private List<MobDetails> mobs;
+
+    @Data
+    public static class MobDetails {
+
+        private String name;
+        private int[] levelRange;
+        private String spawnArea;
+        private String description;
+        private String image;
+        private double healthFactor;
+        private double attackFactor;
+        private double defenseFactor;
+        private int[] adenaDrop;
+        private int[] xpDrop;
+    }
+
+
+}
